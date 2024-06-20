1db787a81478d3c77fec16225e496646d774e1f7
ptthao0801 <ptthao0801@gmail.com>
2024-06-21 01:10:15 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const fruits = [
+    "Apple",
+    "Banana",
+    "Orange",
+    "Apricot",
+    "Black rowan",
+    "Cranberry"
+];
+let element =
+    <div>
+        <h1>List of fruits</h1>
+        <ul>
+            {fruits.map(item => (
+                <li>{item }</li>
+            ))}
+        </ul>
+    </div>
+    
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    element
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
